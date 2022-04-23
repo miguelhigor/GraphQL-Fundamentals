de6042d9ee918b27638efdc6360548b18f6f90ba
Higor Miguel <higor.mig@gmail.com>
2022-04-23 18:04:58 -0300

    feat: list data on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,30 @@
-import { useState } from 'react'
+import { gql, useQuery } from "@apollo/client"
+
+type User = {
+  id: string;
+  name: string;
+}
+
+const GET_USERS = gql`
+  query {
+    users {
+      id,
+      name
+    }
+  }
+`;
 
 function App() {
+  const { data, loading } = useQuery<{ users: User[] }>(GET_USERS);
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
   return (
-    <h1>Hello World</h1>
-  )
+    <ul>
+      {data?.users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
+  );
 }
 
 export default App
